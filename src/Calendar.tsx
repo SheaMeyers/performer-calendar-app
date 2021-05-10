628045ba38c56b7cbeb7bfaa524036ccda171ecf
Shea Meyers <shea.meyers@gmail.com>
2021-05-10 21:12:18 +0200

    Colour code events

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -4,7 +4,31 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import myEventsList from './exampleEvents';
 
 
+interface Event {
+    id: number;
+    title: string;
+    start: Date;
+    end: Date;
+    hexColor: string;
+}
+
 const ReactBigCalendar = () => {
+    const eventStyleGetter = (event: Event) => {
+        var backgroundColor = '#' + event.hexColor;
+        var style = {
+            backgroundColor: backgroundColor,
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'black',
+            border: '0px',
+            display: 'block'
+        };
+        return {
+            style: style
+        };
+
+    }
+
     return (
         <div>
             <Calendar
@@ -13,6 +37,7 @@ const ReactBigCalendar = () => {
                 startAccessor="start"
                 endAccessor="end"
                 style={{ height: 500 }}
+                eventPropGetter={eventStyleGetter}
             />
         </div>
     )
