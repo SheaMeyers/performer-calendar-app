deb052a05dc68b38f1c768a376f35eb4650e6fec
Shea Meyers <shea.meyers@gmail.com>
2021-05-21 08:24:27 +0200

    Style update password and account settings with material ui cards

--- src/components/AccountSettings.tsx
+++ src/components/AccountSettings.tsx
@@ -1,41 +1,52 @@
 import React, { useState } from 'react';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import '../css/AccountSettings.css';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Paper from '@material-ui/core/Paper';
 import SearchPerformers from './SearchPerformers';
 import UserPerformersList from './UserPerformersList';
+import '../css/AccountSettings.css';
 
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    paper: {
-      padding: '1rem',
-      background: 'lightgrey'
-    },
-  }),
+    createStyles({
+        button: {
+            marginTop: '1rem'
+        },
+        cardContent: {
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        paper: {
+            padding: '1rem',
+            background: 'lightgrey'
+        },
+    }),
 );
 
 
 const AccountSettings = () => {
     const [feedbackMessage, setFeedbackMessage] = useState<string>('');
     const classes = useStyles();
-    
+
     return (
         <div className="Account-Settings-Container">
-            <div className="Account-Settings">
-                <h2>Account Settings</h2>
-                <Paper className={classes.paper}>
-                    <SearchPerformers />
-                    <UserPerformersList />
-                </Paper>
-                <Button variant="contained">
-                    Change Password
-                </Button>
-                <Button variant="contained" color="primary">
-                    Logout
-                </Button>
-            </div>
+            <Card>
+                <CardContent className={classes.cardContent}>
+                    <h2>Account Settings</h2>
+                    <Paper className={classes.paper}>
+                        <SearchPerformers />
+                        <UserPerformersList />
+                    </Paper>
+                    <Button variant="contained" className={classes.button}>
+                        Change Password
+                    </Button>
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        Logout
+                    </Button>
+                </CardContent>
+            </Card>
         </div>
     )
 
--- src/components/UpdatePassword.tsx
+++ src/components/UpdatePassword.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import TextField from "@material-ui/core/TextField";
 import '../css/UpdatePassword.css';
 
@@ -9,61 +11,67 @@ const UpdatePassword = () => {
     
     return (
         <div className="Update-Password-Container">
-            <div className="Update-Password">
-                <form onSubmit={event => {
-                    event.persist();
-                    event.preventDefault();
+            <Card>
+                <CardContent>
+                    <form 
+                        className="Update-Password__form"
+                        onSubmit={event => {
+                            event.persist();
+                            event.preventDefault();
 
-                    // @ts-ignore
-                    const elements = event.target.elements;
-                    const email = elements.email.value;
-                    const password = elements.password.value;
-                    const secondPassword = elements.secondPassword.value;
-                    const isSupervisor = elements.checkbox.checked;
+                            // @ts-ignore
+                            const elements = event.target.elements;
+                            const email = elements.email.value;
+                            const password = elements.password.value;
+                            const secondPassword = elements.secondPassword.value;
+                            const isSupervisor = elements.checkbox.checked;
 
-                    const passwordsMatch = password === secondPassword;
-                    const isStrongPassword = password.length > 8 && /\d/.test(password);
+                            const passwordsMatch = password === secondPassword;
+                            const isStrongPassword = password.length > 8 && /\d/.test(password);
 
-                    // if (passwordsMatch && isStrongPassword) {
-                    //     axios.post(`${domainUrl}/backend/sign-up`, 
-                    //         {
-                    //             email: email,
-                    //             password: password,
-                    //         })
-                    //         .then(_ => {setFeedbackMessage("Please check your email for further instructions")
-                    //         })
-                    //         .catch(_ => setFeedbackMessage("Unable to sign up.  Please try again later"));
-                    // } else if (!isStrongPassword) {
-                    //     setFeedbackMessage("Password must be at least 8 characters long with at least one number");
-                    // } else {
-                    //     setFeedbackMessage("Passwords do not match");
-                    // }
-                }}>
-                    <h2>Enter new password</h2>
-                    <TextField
-                        required
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        name="password"
-                        margin="normal"
-                        variant="outlined"
-                    />
-                    <TextField
-                        required
-                        id="outlined-second-password-input"
-                        label="Re-enter Password"
-                        type="password"
-                        name="secondPassword"
-                        margin="normal"
-                        variant="outlined"
-                    />
-                    {feedbackMessage && <p>{feedbackMessage}</p>}
-                    <Button variant="contained" type="submit" color="primary">
-                        Enter
-                    </Button>
-                </form>
-            </div>
+                            // if (passwordsMatch && isStrongPassword) {
+                            //     axios.post(`${domainUrl}/backend/sign-up`, 
+                        //     axios.post(`${domainUrl}/backend/sign-up`, 
+                            //     axios.post(`${domainUrl}/backend/sign-up`, 
+                            //         {
+                            //             email: email,
+                            //             password: password,
+                            //         })
+                            //         .then(_ => {setFeedbackMessage("Please check your email for further instructions")
+                            //         })
+                            //         .catch(_ => setFeedbackMessage("Unable to sign up.  Please try again later"));
+                            // } else if (!isStrongPassword) {
+                            //     setFeedbackMessage("Password must be at least 8 characters long with at least one number");
+                            // } else {
+                            //     setFeedbackMessage("Passwords do not match");
+                            // }
+                    }}>
+                        <h2>Enter new password</h2>
+                        <TextField
+                            required
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            name="password"
+                            margin="normal"
+                            variant="outlined"
+                        />
+                        <TextField
+                            required
+                            id="outlined-second-password-input"
+                            label="Re-enter Password"
+                            type="password"
+                            name="secondPassword"
+                            margin="normal"
+                            variant="outlined"
+                        />
+                        {feedbackMessage && <p>{feedbackMessage}</p>}
+                        <Button variant="contained" type="submit" color="primary">
+                            Enter
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     )
 
