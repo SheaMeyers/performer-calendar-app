72269e5aaaea7b7321c1695c28bec5fd642066e5
Shea Meyers <shea.meyers@gmail.com>
2021-05-23 12:12:40 +0200

    Add logout unit tests

--- backend/tests.py
+++ backend/tests.py
@@ -6,11 +6,8 @@ from backend.models import User
 
 # TODO Add tests for
 #  Sign up/Registration
-#  Login
-#  Logout
 #  Forgot password
 #  Change password
-
 
 
 class LoginTests(TestCase):
@@ -20,7 +17,7 @@ class LoginTests(TestCase):
         user.set_password('password')
         user.save()
 
-    def test_login_works_properly(self):
+    def test_successful_login_creates_token_and_returns_it_in_response(self):
         login_url = reverse('rest_login')
         data = {
             'email': 'email@email.com',
@@ -31,7 +28,7 @@ class LoginTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(Token.objects.filter(key=response.json()['key']).exists())
 
-    def test_login_with_bad_password(self):
+    def test_unsuccessful_login_does_not_create_token(self):
         login_url = reverse('rest_login')
         data = {
             'email': 'email@email.com',
@@ -40,4 +37,19 @@ class LoginTests(TestCase):
         response = self.client.post(login_url, data=data)
 
         self.assertEqual(response.status_code, 400)
+        self.assertFalse(Token.objects.exists())
+
+
+class LogoutTests(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(email='email@email.com')
+        self.token = Token.objects.create(user=self.user)
+        self.client.force_login(self.user)
+
+    def test_logout_deletes_token(self):
+        logout_url = reverse('rest_logout')
+        response = self.client.post(logout_url)
+
+        self.assertEqual(response.status_code, 200)
         self.assertFalse(Token.objects.exists())
