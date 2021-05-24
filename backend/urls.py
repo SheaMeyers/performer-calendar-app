f6a89abac519a48456b502a693f95ff425d1f1fa
Shea Meyers <shea.meyers@gmail.com>
2021-05-24 14:29:45 +0200

    Add django view for resetting password

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from django.urls import include
+from django.urls import include, path
+
+from .views import PasswordResetConfirmView
 
 urlpatterns = [
     url(r'^rest-auth/', include('rest_auth.urls'), name='rest_auth'),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls'), name='rest_auth_authentication'),
+
+    # Override Django's default view to direct into React view
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     url(r'^', include('django.contrib.auth.urls')),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.views.decorators.cache import never_cache
+from django.views.decorators.debug import sensitive_post_parameters
 
-# Create your views here.
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+
+
+UserModel = get_user_model()
+
+
+class PasswordResetConfirmView(APIView):
+
+    @method_decorator(sensitive_post_parameters())
+    @method_decorator(never_cache)
+    def dispatch(self, *args, **kwargs):
+        assert 'uidb64' in kwargs and 'token' in kwargs
+        uid = urlsafe_base64_decode(kwargs['uidb64']).decode()
+        user = UserModel._default_manager.get(pk=uid)
+        user.set_unusable_password()
+        user.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return redirect(f'/update-password?token={urlsafe_base64_encode(bytes(token.key, encoding="UTF-8"))}')
