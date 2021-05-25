41348c406084fea956e418be4d52cb8713691d05
Shea Meyers <shea.meyers@gmail.com>
2021-05-25 08:32:41 +0200

    Add hex color field to performer

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,10 @@
+import random
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+def get_hex_color():
+    return f'#{random.randint(1, 255):02X}{random.randint(1, 255):02X}{random.randint(1, 255):02X}'
 
 
 class User(AbstractUser):
@@ -16,3 +21,4 @@ class Performer(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=280)
     user = models.ManyToManyField(User, related_name='performers')
+    hex_color = models.CharField(max_length=7, default=get_hex_color)
