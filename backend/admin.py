f28a88876a153c591cce58570352008880895a0f
Shea Meyers <shea.meyers@gmail.com>
2021-06-07 18:35:37 +0200

    Add admin models

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Performer
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    search_fields = ('email',)
+
+
+@admin.register(Performer)
+class PerformerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('id', 'name',)
