3e9268f521a8dc916d25a1b6392d5489e9283368
Shea Meyers <shea.meyers@gmail.com>
2021-06-13 20:34:28 +0200

    Prevent crash when clicking to see more

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -65,6 +65,14 @@ const ReactBigCalendar = () => {
         window.open(event.url, "_blank", "noopener");
     }
 
+    const handleOnShowMore = (events: Event[], date: Date) => {
+        // Convert to date objects to prevent crash if user clicks to show more
+        events.map(event => {
+            event.start = new Date(event.start);
+            event.end = new Date(event.end);
+        });
+    }
+
     const classes = useStyles();
 
     return (
@@ -79,6 +87,7 @@ const ReactBigCalendar = () => {
                     tooltipAccessor={eventTooltipGetter}
                     eventPropGetter={eventStyleGetter}
                     onSelectEvent={handleOnSelectEvent}
+                    onShowMore={handleOnShowMore}
                 />
             </CardContent>
         </Card>
