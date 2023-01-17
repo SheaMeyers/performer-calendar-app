c4f15f8cd284da710fcacaf7c4e5f27fd5b3a778
Shea Meyers <shea.meyers@gmail.com>
2023-01-17 18:14:19 +0100

    Get working with ticketmaster api

--- src/api.ts
+++ src/api.ts
@@ -1,44 +1,29 @@
 import axios from "axios"
-import { Performer, ShowPerformer, Event } from "./interfaces"
+import { Event } from "./interfaces"
 
-export const SEATGEEK_ENDPOINT = 'https://api.seatgeek.com/2/'
+export const TICKETMASTER_ENDPOINT = 'https://app.ticketmaster.com/discovery/v2/'
 
 // Note: While having an API Key publicly available is generally a bad practice, 
-//   this is a free key that anyone can get from SeatGeek and so there is really
+//   this is a free key that anyone can get from Ticketmaster and so there is really
 //   no point in stealing it.  This also allows anyone cloning the repo to more
 //   easily test the code out.
-export const SEATGEEK_APIKEY = 'MjE5Mjg3MTN8MTYyMTA3ODMzNC40NDc3NDI1'
+export const TICKETMASTER_APIKEY = 'OIcjO8xAGvyfG7rv47wVPnh5O3IdCO4G'
 
-export const getPerformers = async (query: string) => {
-    try {
-        const response = await axios.get(`${SEATGEEK_ENDPOINT}performers?q=${query}&client_id=${SEATGEEK_APIKEY}`)
-        return response.data.performers.map((performer: any): ShowPerformer => { 
-            return { 
-                id: performer.id,
-                name: performer.name,
-                hexColor: `#${Math.floor(Math.random()*16777215).toString(16)}`,
-                showCheckbox: true
-            }
-        })
-    } catch (e) {
-        return []
-    }
-}
 
-export const getPerformerEvents = async (performer: Performer) => {
+export const getEvents = async (keyword: string, hexColor: string): Promise<Event[]> => {
+    const events: Event[] = []
     try {
-        const response = await axios.get(`${SEATGEEK_ENDPOINT}events?performers.id=${performer.id}&client_id=${SEATGEEK_APIKEY}`)
-        return response.data.events.map((event: any): Event => {
-            return {
+        const response = await axios.get(`${TICKETMASTER_ENDPOINT}events.json?keyword=${keyword}&apikey=${TICKETMASTER_APIKEY}`)
+        response.data._embedded.events.forEach((event: any): void => {
+            events.push({
                 id: event.id,
-                title: performer.name,
-                start: event.datetime_local,
-                end: event.datetime_local.split('T')[0] + 'T23:59:59',
-                hexColor: performer.hexColor,
+                title: event.name,
+                start: event.dates.start.dateTime,
+                end: event.dates.start.dateTime.split('T')[0] + 'T23:59:59',
+                hexColor,
                 url: event.url
-            }
+            })
         })
-    } catch (e) {
-        return []
-    }
+    } catch (e) { }
+    return events
 }
